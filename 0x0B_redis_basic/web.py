a457b0252c6e980ebcc5cb4b64baded1043cbc64
root <g.munezero1@alustudent.com>
2025-06-10 08:29:16 +0000

    web.py

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -1,25 +1,38 @@
 #!/usr/bin/env python3
-"""Implementing an expiring web cache and tracker"""
-from redis.client import Redis
+'''A module with tools for request caching and tracking.
+'''
+import redis
 import requests
+from functools import wraps
+from typing import Callable
 
 
-redis = Redis()
-count = 0
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
 def get_page(url: str) -> str:
-    """
-    uses the requests module to obtain
-    the HTML content of a particular URL and returns it.
-    """
-    data = f"count:{url}"
-    redis.set(data, count)
-    res = requests.get(url)
-    redis.incr(data)
-    redis.setex(data, 10, redis.get(data))
-    return res.text
-
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
