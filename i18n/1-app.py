1cdf0394180c7c9207f851ea726008e6a5152551
root <g.munezero1@alustudent.com>
2025-06-11 07:30:46 +0000

    i18n files

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -1,33 +1,27 @@
 #!/usr/bin/env python3
-"""
-0x0A. i18n
-"""
-from os import getenv
+'''Basic Flask app'''
+
 from flask import Flask, render_template
 from flask_babel import Babel
 
 
-app = Flask(__name__)
-babel = Babel(app)
-
-
-class Config(object):
-    """class config"""
+class Config:
+    '''Flask babel app config'''
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route('/')
-def index():
-    """hello world"""
-    return render_template("1-index.html", message="Welcome to Holberton")
+def hello_world():
+    '''Basic Flask app'''
+    return render_template('1-index.html')
 
 
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
