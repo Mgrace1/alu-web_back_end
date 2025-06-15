83a1da797e70c0f124028471ebee7676dea2a502
root <g.munezero1@alustudent.com>
2025-06-15 18:16:14 +0000

    i18n files

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,48 +1,44 @@
 #!/usr/bin/env python3
 """
-A Basic flask application
+Route module for the API
 """
-from flask import Flask
-from flask import request
-from flask import render_template
+from flask import Flask, render_template, request
+from os import getenv
 from flask_babel import Babel
 
 
-class Config(object):
-    """
-    Application configuration class
-    """
+app = Flask(__name__)
+
+
+class Config:
+    """ class config"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-# Instantiate the application object
-app = Flask(__name__)
 app.config.from_object(Config)
-
-# Wrap the application with Babel
 babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """
-    Gets locale from request object
-    """
-    locale = request.args.get('locale', '').strip()
-    if locale and locale in Config.LANGUAGES:
-        return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+def get_locale():
+    """best match locale lang"""
+    local_lang = request.args.get('locale')
+    support_lang = app.config['LANGUAGES']
+    if local_lang in support_lang:
+        return local_lang
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Renders a basic html template
-    """
-    return render_template('4-index.html')
+@app.route('/')
+def index():
+    """hello world"""
+    return render_template("4-index.html", message="Welcome to Holberton")
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
